265c424eb9be0380de9c99029ef82ab01b677e14
Malene Klit <maleneklit@hotmail.com>
2020-01-06 08:49:34 +0100

    add editing option on all fields

--- base.js
+++ base.js
@@ -6,22 +6,73 @@ let app = new Vue({
         newTodo: '',
         todos: [],
         newGoal: '',
+        editedObj: '',
         prioGoals: [],
         errors: {
             todos: '',
             prioGoals: '',
         },
         hours: {
-            1: null,
-            2: null,
-            3: null,
-            4: null,
-            5: null,
-            6: null,
-            7: null,
-            8: null,
-            9: null,
-            10: null,
+            1: {
+                time: '08-09',
+                value: '',
+                done: false,
+                edit: true
+            },
+            2: {
+                time: '09-10',
+                value: '',
+                done: false,
+                edit: true
+            },
+            3: {
+                time: '10-11',
+                value: '',
+                done: false,
+                edit: true
+            },
+            4: {
+                time: '11-12',
+                value: '',
+                done: false,
+                edit: true
+            },
+            5: {
+                time: '12-13',
+                value: '',
+                done: false,
+                edit: true
+            },
+            6: {
+                time: '13-14',
+                value: '',
+                done: false,
+                edit: true
+            },
+            7: {
+                time: '14-15',
+                value: '',
+                done: false,
+                edit: true
+            },
+            8: {
+                time: '15-16',
+                value: '',
+                done: false,
+                edit: true
+            },
+            9: {
+                time: '16-17',
+                value: '',
+                done: false,
+                edit: true
+            },
+            10: {
+                time: '17-18',
+                value: '',
+                done: false,
+                edit: true
+            }
         },
         tab: 'todo'
     },
@@ -43,8 +94,8 @@ let app = new Vue({
           });
           this.prioGoals = prioGoals;
 
-          for( let i = 1; i <= 10; i++) {
-              this.hours[i] = JSON.parse( localStorage.getItem('todo_app_hours_' + i) || null);
+          for( let i = 1; i <= 2; i++) {
+              this.hours[i] = JSON.parse( localStorage.getItem('todo_app_hours_' + i) || []);
           }
       })
     },
@@ -78,16 +129,20 @@ let app = new Vue({
     },
     methods: {
         addTodo: function () {
-            if(this.todos.length >= 5){
-                this.errors.todos = 'You can only add 5 main todos, delete a todo to add a new todo';
-                return
+            //Alow only 5 todos each day
+            // if(this.todos.length >= 5){
+            //     this.errors.todos = 'You can only add 5 main todos, delete a todo to add a new todo';
+            //     return
+            // }
+            if(this.newTodo){
+                let value = this.newTodo;
+                this.todos.push({
+                    value: value,
+                    done: false,
+                    edit: false
+                });
+                this.newTodo = '';
             }
-            let value = this.newTodo;
-            this.todos.push({
-                value: value,
-                done: false
-            });
-            this.newTodo = '';
         },
         removeTodo: function(todo) {
             this.todos.splice(this.todos.indexOf(todo), 1);
@@ -97,24 +152,22 @@ let app = new Vue({
                 this.errors.prioGoals = 'You can only add 3 main goals, delete a goal to add a new goal';
                 return
             }
-            let value = this.newGoal;
-            this.prioGoals.push({
-                value: value,
-                done: false
-            });
-            this.newGoal = '';
+            if(this.newGoal) {
+                let value = this.newGoal;
+                this.prioGoals.push({
+                    value: value,
+                    done: false,
+                    edit: false
+                });
+                this.newGoal = '';
+            }
         },
         removeGoal: function(prioGoal) {
             this.prioGoals.splice(this.prioGoals.indexOf(prioGoal), 1);
         },
-        addHourTodo: function(hour, value){
-            this.hours[hour] = {
-                value: value,
-                done: false
-            };
-        },
-        removeHourTodo: function(hour){
-            this.hours[hour] = null;
+        removeHourTodo: function(key){
+            this.hours[key].value = '';
+
         },
         saveTodosToLocalStorage: function (todos) {
             localStorage.setItem(STORAGE_KEY_TODOS, JSON.stringify(todos));
@@ -123,8 +176,48 @@ let app = new Vue({
             localStorage.setItem(STORAGE_KEY_GOAL, JSON.stringify(goals));
         },
         saveHoursToLocalStorage: function (hours) {
-            for( let i = 1; i <= 10; i++) {
+
+            for( let i = 1; i <= 2; i++) {
                 localStorage.setItem('todo_app_hours_' + i, JSON.stringify(hours[i]))
+            }
+        },
+        edit: function(obj){
+            //If object is alrady done - its should not be possible to edit the object any more
+            if(obj.done){
+                return;
+            }
+            obj.edit = true;
+            this.beforeEditCasch = obj.value;
+            this.editedObj = obj;
+
+        },
+        doneEdit: function (obj) {
+            //if nothing is entered into the input field - we don't want to do anything (something has to be typed first)
+            if(obj.value.length === 0){
+                return;
+            }
+            if(!this.editedObj){
+                obj.edit = false;
+                return
+            }
+            this.editedObj = '';
+            obj.edit = false;
+
+            if(!obj.value){
+                this.removeGoal(obj);
+            }
+        },
+        cancelEdit: function (obj) {
+            obj.edit = false;
+            obj.value = this.beforeEditCasch;
+        }
+    },
+    // a custom directive to wait for the DOM to be updated
+    // before focusing on the input field.
+    directives: {
+        'goal-focus': function (el, binding) {
+            if (binding.value) {
+                el.focus();
             }
         }
     }
